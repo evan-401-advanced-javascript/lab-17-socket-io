e661cc2de09b9f641c6bc7eadfe8d4b126efa680
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-04 17:48:49 -0700

    fixed to uppercase

--- app/app.js
+++ app/app.js
@@ -10,9 +10,9 @@ const fs = require('fs');
  * Function uses fs to read file and then calls change case
  * @param file
  */
-const readFile = (file) => {
+const readFile = async (file) => {
   let text = {};
-  fs.readFile( file, (err, data) => {
+  await fs.readFile( file, (err, data) => {
     if(err) {server.emit('file-error')}
     text.data = data.toString();
     text.file = file;
@@ -26,8 +26,8 @@ const readFile = (file) => {
  * @param text
  */
 const changeCase = (text) => {
-  text.data.toUpperCase();
-  console.log('file changed to upper case');
+  text.data = text.data.toUpperCase();
+  console.log('file changed to upper case', text.data);
   updatingFile(text);
 };
 
@@ -35,8 +35,8 @@ const changeCase = (text) => {
  * function uses fs to write the changes to the file and then uses socket to call server.js
  * @param text
  */
-const updatingFile = (text) => {
-  fs.writeFile( text.file, Buffer.from(text.data), (err, data) => {
+const updatingFile = async (text) => {
+  await fs.writeFile( text.file, Buffer.from(text.data), (err, data) => {
     if(err) {server.emit('file-error')}
     console.log('file updated');
     server.emit('file-save', `${file} saved`);
